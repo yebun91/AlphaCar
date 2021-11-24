8c6dc87283367d4dcfb103704c3d1b18666f7092
yebun91 <65695703+yebun91@users.noreply.github.com>
2021-11-24 12:36:32 +0900

    html 삭제

--- AlphaCar_Server/ateam.sql
+++ AlphaCar_Server/ateam.sql
@@ -8,3 +8,8 @@ INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content,
 VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목4', 'test 내용4', 'N');
 INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
 VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목5', 'test 내용5', 'N');
+INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
+VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5test 제목5', 
+  'test 내용5', 'A');
+  
+commit;
