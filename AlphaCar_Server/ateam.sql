42dd812b3f6cb3db6c937c259ad289d1792514fb
yebun91 <65695703+yebun91@users.noreply.github.com>
2021-11-24 11:20:41 +0900

    sql문

--- AlphaCar_Server/ateam.sql
+++ AlphaCar_Server/ateam.sql
@@ -1,2 +1,10 @@
-INSERT INTO web_notice (customer_email, notice_title, notice_content) 
-VALUES (web_notice.nextval,'admin','test 제목', 'test 내용')
+INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
+VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목', 'test 내용', 'N');
+INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
+VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목2', 'test 내용2', 'N');
+INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
+VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목3', 'test 내용3', 'N');
+INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
+VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목4', 'test 내용4', 'N');
+INSERT INTO web_notice (notice_id, customer_email, notice_title, notice_content, notice_attribute) 
+VALUES (SEQ_WEB_NOTICE.nextval, 'admin@naver.com', 'test 제목5', 'test 내용5', 'N');
