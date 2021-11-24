056ba13d334be22eea3fac902a4e22f15e891010
holic4570 <ghkdrkdmf0@gmail.com>
2021-11-24 15:28:02 +0900

    no message

--- AlphaCar_Server/workspace/alphacar/src/main/java/com/hanul/alphacar/HomeMemberController.java
+++ AlphaCar_Server/workspace/alphacar/src/main/java/com/hanul/alphacar/HomeMemberController.java
@@ -1,15 +1,54 @@
 package com.hanul.alphacar;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import member.MemberServiceImpl;
+import member.MemberVO;
+import member.WebMemberVO;
 
 @Controller
 public class HomeMemberController {
+	
+	@Autowired private MemberServiceImpl service;
+	
+	// 로그아웃 처리 요청
+	@RequestMapping ("/homeLogout")
+	public String logout (HttpSession session) {
+		// 세션에 담긴 로그인 정보를 삭제한다.
+		session.removeAttribute("loginInfo");
+		
+		// 로그아웃 시 루트(home.jsp)로 이동
+		return "redirect:/";	
+	}
+	
+	//로그인 처리 요청
+	@ResponseBody
+	@RequestMapping("/webLogin")
+	public Boolean webLogin(HttpSession session, String customer_email, String customer_pw) {
+		System.out.println(customer_email);
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("customer_email", customer_email);
+		map.put("customer_pw", customer_pw);
+		WebMemberVO vo = service.member_login(map);
+		session.setAttribute("loginInfo", vo);
+		
+		return vo == null ? false : true;
+	}
+	
+	//로그인 화면 요청
 	@RequestMapping("/homeLogin")
 	public String login(HttpSession session, Model model) {
+		
+		session.setAttribute("category", "homeLogin");
 		return "member/login";
 	}
 	
--- AlphaCar_Server/workspace/alphacar/src/main/java/com/hanul/alphacar/ListController.java
+++ AlphaCar_Server/workspace/alphacar/src/main/java/com/hanul/alphacar/ListController.java
@@ -1,8 +1,11 @@
 package com.hanul.alphacar;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
 import com.google.gson.Gson;
 
+import store.RegisterVO;
+import store.StoreFileVO;
 import store.StoreServiceImpl;
 import store.StoreVO;
 
@@ -24,22 +30,127 @@ public class ListController {
 
 	@Autowired private StoreServiceImpl service;
 	
-//	@ResponseBody
-//	@RequestMapping("anSelectDetail")
-//	public void anSelectDetail(HttpServletRequest req, HttpServletResponse res, int store_number) throws IOException {
-//		res.setCharacterEncoding("UTF-8");
-//		res.setContentType("text/html");
-//		req.setCharacterEncoding("UTF-8");
-//		
-//		StoreVO vo = service.store_detail(store_number);
-//		
-//
-//		PrintWriter out = res.getWriter();
-//		Gson gson = new Gson();
-//		String data = gson.toJson(vo);
-//		out.println(data);
-//		
-//	}
+	
+	@ResponseBody
+	@RequestMapping("/anStoreRegister")
+	public void anStoreRegister(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html");
+		req.setCharacterEncoding("UTF-8");
+		System.out.println(req.getParameter("customer_email"));
+		System.out.println(req.getParameter("store_addr"));
+		System.out.println(req.getParameter("inventory"));
+		
+		ArrayList<String> storeInventory = new ArrayList<String>();
+		for (int i =0; i< 9; i++){
+            storeInventory.add("X");
+        }
+        for(int i =0; i<Integer.parseInt(req.getParameter("inventory")); i++){
+            storeInventory.set(i, "Y");
+        }
+		
+		
+		String fileName = "";
+		String realImgPath = "";
+		
+		RegisterVO vo = new RegisterVO();
+		vo.setCustomer_email(req.getParameter("customer_email"));
+		vo.setStore_name(req.getParameter("store_name"));
+		vo.setStore_addr(req.getParameter("store_addr"));
+		vo.setStore_tel(req.getParameter("store_tel"));
+		vo.setStore_time(req.getParameter("store_time"));
+		vo.setStore_dayoff(req.getParameter("store_dayoff"));
+		vo.setIntroduce(req.getParameter("introduce"));
+		vo.setInventory(Integer.parseInt(req.getParameter("inventory")));
+		
+		for (int i = 0; i < storeInventory.size(); i++) {
+			vo.setNow_state(storeInventory.get(i));
+			
+		}
+		
+
+		vo.setStore_price(req.getParameter("store_price"));
+		vo.setStore_master_name(req.getParameter("store_master_name"));
+		vo.setStore_registration_number(req.getParameter("store_registration_number"));
+		
+		
+		MultipartRequest multi = (MultipartRequest)req;
+		
+		ArrayList<MultipartFile> file = new ArrayList<MultipartFile>();
+		file.add(multi.getFile("imgpath1"));
+		file.add(multi.getFile("imgpath2"));
+		file.add(multi.getFile("imgpath3"));
+		
+		for (int i = 0; i < file.size(); i++) {
+			if(file.get(i) != null) {
+				fileName = file.get(i).getOriginalFilename();
+				System.out.println("fileName : " + fileName);
+				
+				if(file.get(i).getSize() > 0) {
+					realImgPath = req.getSession().getServletContext()
+							.getRealPath("/resources/");
+					
+					System.out.println("realpath : " + realImgPath);
+					System.out.println("fileSize : " + file.get(i).getSize());
+					
+					// Ïù¥ÎØ∏ÏßÄ ÌååÏùºÏùÑ ÏÑúÎ≤ÑÏóê Ï†ÄÏû•
+					try {
+						file.get(i).transferTo(new File(realImgPath, fileName));
+					} catch (Exception e) {
+						e.getMessage();
+					} 
+					
+				}
+			
+			
+			}
+			if (i==0) {
+				vo.setImgname1(fileName);
+				vo.setImgpath1(realImgPath);
+			}else if(i==1) {
+
+				vo.setImgname2(fileName);
+				vo.setImgpath2(realImgPath);
+			}else if(i==2) {
+
+				vo.setImgname3(fileName);
+				vo.setImgpath3(realImgPath);
+			}
+		}
+	
+		int result = service.store_file_register(vo);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("result", result +"");
+		PrintWriter out = res.getWriter();
+		Gson gson = new Gson();
+		String data = gson.toJson(map);
+		out.println(data);
+
+	}
+	
+	@ResponseBody
+	@RequestMapping("/anSelectAllDetail")
+	public void anSelectAllDetail(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html");
+		req.setCharacterEncoding("UTF-8");
+		String param = req.getParameter("customer_email");
+		System.out.println(param);
+		
+		List<StoreVO> list = service.store_state(param);
+		
+		System.out.println(list.size());
+		
+		
+		PrintWriter out = res.getWriter();
+		Gson gson = new Gson();
+		String data = gson.toJson(list);
+		out.println(data);
+		
+	}
+	
 	@ResponseBody
 	@RequestMapping("/anSelectDetail")
 	//@RequestMapping(value ="anSelectDetail", method = {RequestMethod.GET, RequestMethod.POST})
@@ -60,7 +171,7 @@ public class ListController {
 	}
 	
 	
-	//¿¸√º ∏ÆΩ∫∆Æ ¡∂»∏
+	
 	@ResponseBody
 	@RequestMapping("anSelectMulti")
 	//@RequestMapping(value ="anSelectMulti", method = {RequestMethod.GET, RequestMethod.POST})
@@ -77,5 +188,16 @@ public class ListController {
 		out.println(data);
 		
 		//return data;
+	}
+	
+	public void common(HttpServletResponse res) throws IOException {
+		List<StoreVO> list = null;
+		
+		
+		PrintWriter out = res.getWriter();
+		Gson gson = new Gson();
+		String data = gson.toJson(list);
+		out.println(data);
+		
 	}
 }
