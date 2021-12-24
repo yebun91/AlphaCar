2d123699771de43d0a9e8214de65662e5b5f2fba
holic4570 <ghkdrkdmf0@gmail.com>
2021-12-24 11:22:42 +0900

    합합

--- AlphaCar_Spring/workspace/alphacar/src/main/java/homeChart/ChartVO.java
+++ AlphaCar_Spring/workspace/alphacar/src/main/java/homeChart/ChartVO.java
@@ -4,9 +4,47 @@ package homeChart;
 public class ChartVO{
    
    private int tt;
+   private int time;
+   private int cnt;
+   private int dcode;
+   private String customer;
+   
+    
+	public String getCustomer() {
+		return customer;
+	}
 
-   public int getTt() {
-      return tt;
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+	public int getDcode() {
+		return dcode;
+	}
+	
+	public void setDcode(int dcode) {
+		this.dcode = dcode;
+	}
+	
+	
+	public int getTime() {
+		   return time;
+	}
+	
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	public int getCnt() {
+		return cnt;
+	}
+	
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	public int getTt() {
+	      return tt;
    }
 
    public void setTt(int tt) {
